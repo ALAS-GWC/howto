210b79a6d0eba207908dcd6e155a97f5f58f962e
lydiakim143 <lydiakim143@gmail.com>
2017-08-23 10:10:58 -0400

    reorganize pages

--- howto/views.py
+++ howto/views.py
@@ -7,16 +7,19 @@ def index(request):
     # this is your new view
     return render(request, 'index.html')
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+def world(request):
+    return render(request, 'world.html')
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('home')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'signup.html', {'form': form})
--- howtoapp/urls.py
+++ howtoapp/urls.py
@@ -4,6 +4,7 @@ from howto import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^signup/$', views.signup, name='signup'),
+    # url(r'^signup/$', views.signup, name='signup'),
+    url(r'^world/$', views.world, name='world'),
     url(r'^admin/', admin.site.urls),
 ]
